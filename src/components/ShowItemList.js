ba3936c11fd8a33dc7132aacee5a60f1f4ae194d
abigailNottingham <abigailillustration@gmail.com>
2021-07-12 20:03:03 +0100

    imported firestore to ShowItemList component

--- src/components/ShowItemList.js
+++ src/components/ShowItemList.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
-//import firebase from 'firebase/app';
-//import { useCollection } from 'react-firebase-hooks/firestore';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import { useCollection } from 'react-firebase-hooks/firestore';
+
+import '../lib/firebase';
+//const db = fb.firestore()
 
 function ShowItemList() {
-  /*     const [value, loading, error] = useCollection(
-            firebase.firestore().collection('items')  
-        ); */
+  const [value, loading, error] = useCollection(
+    firebase.firestore().collection('items'),
+  );
 
   return (
     <div>
-      {/* {error && <p>"Error"</p>}
-            {loading && <p>"Loading.."</p>}
-            {value && 
-                <p>"value"</p>
-            } */}
+      {error && <p>"Error"</p>}
+      {loading && <p>"Loading.."</p>}
+      {value && <p>"value"</p>}
     </div>
   );
 }
