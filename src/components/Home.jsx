78eb1ef0a922efb9b50a796c9f5b8f8e62e59a1f
blackbureau <yspol.uk@gmail.com>
2021-07-19 16:08:23 +0100

    create token and store it in local storage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import getToken from '../lib/tokens';
 
 const Home = () => {
-  return <h1>Home</h1>;
+  const Token = function generateToken() {
+    const token = getToken();
+    localStorage.setItem('token', token);
+  };
+
+  /*   function fetchToken() {
+    localStorage.getItem('token');
+  } */
+
+  return (
+    <div>
+      <button onClick={Token}>Create a new list</button>
+    </div>
+  );
 };
 
 export default Home;
