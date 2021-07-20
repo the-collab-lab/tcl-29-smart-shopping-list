e01c45ad31064160d4a4a250bc894e582adc9bea
blackbureau <yspol.uk@gmail.com>
2021-07-20 16:32:20 +0100

    generate new token & save to localStorage. Create <input> to verify existing token

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import getToken from '../lib/tokens';
+import { useHistory } from 'react-router-dom';
 
 const Home = () => {
+  let history = useHistory();
+
   const Token = function generateToken() {
     const token = getToken();
     localStorage.setItem('token', token);
+    history.push('/list-view');
+
+    console.log(token);
   };
 
-  /*   function fetchToken() {
+  function fetchToken() {
     localStorage.getItem('token');
-  } */
+    history.push('/list-view');
+  }
 
   return (
     <div>
       <button onClick={Token}>Create a new list</button>
+
+      <input
+        placeholder="three work token"
+        for="inputToken"
+        onChange={fetchToken}
+      ></input>
     </div>
   );
 };
