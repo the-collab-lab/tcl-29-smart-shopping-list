4b80d885591e77ee23077a5dba161b04b000395d
abigailNottingham <abigailillustration@gmail.com>
2021-07-20 18:45:48 +0100

    added state change to form inputs, added checked attribute to all radio buttons & set form submit function to retrieve token and send data

--- src/components/CreateItem.jsx
+++ src/components/CreateItem.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import db from '../lib/firebase';
 import './createItem.css';
 
 function CreateItem() {
-  const handleSubmit = async () => {
-    console.log('submitted');
+  const itemObj = {
+    itemName: '',
+    frequency: 7,
+  };
+  const [item, setItem] = useState(itemObj);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // localStorage.setItem('token', 'a tester token');
+    const token = localStorage.getItem('token');
+    console.log(token);
+    await db.collection('items').add({
+      name: item.itemName,
+      frequency: item.frequency,
+      lastPurchaseDate: null,
+      date: Date().toLocaleString(),
+    });
+    setItem(itemObj);
   };
 
-  // const handleClick = async () => {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setItem({
+      ...item,
+      [name]: value,
+    });
+
+    console.log(item);
+  };
+
+  // constg handleClick = async () => {
   //   await db.collection('items').add({
   //     name: 'A test product',
   //     date: Date().toLocaleString(),
@@ -18,23 +44,50 @@ function CreateItem() {
     <div>
       <h1>AddView</h1>
 
-      <form onSubmit={() => handleSubmit()}>
+      <form onSubmit={handleSubmit}>
         <label HTMLfor="item-name">Item name:</label>
-        <input type="text" id="item-name" value="" />
+        <input
+          type="text"
+          id="item-name"
+          value={item.itemName}
+          name="itemName"
+          onChange={handleChange}
+        />
         <fieldset className="radio-buttons">
           <legend>How soon will you buy this again?</legend>
           <div>
-            <input type="radio" id="soon" value={7} name="frequency" checked />
+            <input
+              type="radio"
+              id="soon"
+              value={7}
+              name="frequency"
+              onChange={handleChange}
+              checked={item.frequency === '7'}
+            />
             <label HTMLfor="soon">Soon</label>
           </div>
 
           <div>
-            <input type="radio" id="kind-of-soon" value={14} name="frequency" />
+            <input
+              type="radio"
+              id="kind-of-soon"
+              value={14}
+              name="frequency"
+              onChange={handleChange}
+              checked={item.frequency === '14'}
+            />
             <label HTMLfor="kind-of-soon">Kind of soon</label>
           </div>
 
           <div>
-            <input type="radio" id="not-soon" value={30} name="frequency" />
+            <input
+              type="radio"
+              id="not-soon"
+              value={30}
+              name="frequency"
+              onChange={handleChange}
+              checked={item.frequency === '30'}
+            />
             <label HTMLfor="not-soon">Not soon</label>
           </div>
         </fieldset>
