56666210199c8db0dfff857fc5d9cc041816a256
blackbureau <yspol.uk@gmail.com>
2021-07-22 16:15:30 +0100

    once token is present in local storage, user is immediately taken to list-view

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import getToken from '../lib/tokens';
+import { Redirect } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
 
 const Home = () => {
+  // const [hasToken, setHasToken] = useState(false);
+
   let history = useHistory();
 
   const Token = function generateToken() {
@@ -13,20 +16,27 @@ const Home = () => {
     console.log(token);
   };
 
-  function fetchToken() {
-    localStorage.getItem('token');
-    history.push('/list-view');
-  }
+  // function fetchToken() {
+  //   localStorage.getItem('token');
+  //   history.push('/list-view');
+  // }
+
+  const existingToken = localStorage.getItem('token');
 
   return (
     <div>
       <button onClick={Token}>Create a new list</button>
-
-      <input
+      {existingToken ? <Redirect from="/" to="/list-view" /> : ''}
+      {
+        {
+          /* <input> below will be used at a later date  */
+        }
+        /* <input
         placeholder="three work token"
         for="inputToken"
         onChange={fetchToken}
-      ></input>
+      ></input> */
+      }
     </div>
   );
 };
