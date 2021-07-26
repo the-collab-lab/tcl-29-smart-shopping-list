39e8ba2e4ae23b2edc78da1e0b649865d7672c33
abigailNottingham <abigailillustration@gmail.com>
2021-07-26 17:21:29 +0100

    added styling to Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import getToken from '../lib/tokens';
 import { useHistory } from 'react-router-dom';
 
+import './Home.css';
+
 const Home = () => {
   let history = useHistory();
   const [tokenName, setTokenName] = useState('');
@@ -13,21 +15,26 @@ const Home = () => {
   };
 
   return (
-    <div>
+    <div class="home">
       <h1>Welcome to your Smart Shopping list!</h1>
       <button onClick={generateToken}>Create a new list</button>
       <p> - or - </p>
       <p>Join an existing shopping list by entering a three word token.</p>
 
       <form>
-        <label htmlFor="inputToken">Share token</label>
+        <label htmlFor="inputToken" className="shareToken">
+          Share token
+        </label>
         <input
+          className="inputBox"
           placeholder="three work token"
           id="inputToken"
           value={tokenName}
           onChange={(e) => setTokenName(e.target.value)}
         />
-        <button type="submit">Join an existing list</button>
+        <button type="submit" className="submitButton">
+          Join an existing list
+        </button>
       </form>
     </div>
   );
