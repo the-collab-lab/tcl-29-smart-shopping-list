b49c600770b861340f708d2889a76082ca0aef60
abigailNottingham <abigailillustration@gmail.com>
2021-07-26 16:58:34 +0100

    added jsx content for form and user home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import getToken from '../lib/tokens';
 import { useHistory } from 'react-router-dom';
 
 const Home = () => {
   let history = useHistory();
+  const [tokenName, setTokenName] = useState('');
 
   const generateToken = () => {
     const token = getToken();
@@ -13,9 +14,25 @@ const Home = () => {
 
   return (
     <div>
+      <h1>Welcome to your Smart Shopping list!</h1>
       <button onClick={generateToken}>Create a new list</button>
+      <p> - or - </p>
+      <p>Join an existing shopping list by entering a three word token.</p>
+
+      <form>
+        <label htmlFor="inputToken">Share token</label>
+        <input
+          placeholder="three work token"
+          id="inputToken"
+          value={tokenName}
+          onChange={(e) => setTokenName(e.target.value)}
+        />
+        <button type="submit">Join an existing list</button>
+      </form>
     </div>
   );
 };
+
+/*  */
 
 export default Home;
