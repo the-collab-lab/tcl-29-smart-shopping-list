cacd7d36eb5a240aced751382c30bc2877e69bab
abigailNottingham <abigailillustration@gmail.com>
2021-08-02 18:27:18 +0100

    added conditional rendering statement if list is empty so a button is shown that take user to add view.

--- src/components/ShowList.jsx
+++ src/components/ShowList.jsx
@@ -2,19 +2,31 @@ import React from 'react';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import db from '../lib/firebase';
 import Navigation from './Navigation';
+import { useHistory } from 'react-router-dom';
 
 function ShowList() {
   const token = localStorage.getItem('token');
+  const history = useHistory();
   const [value, loading, error] = useCollection(
     db.collection('items').where('token', '==', token),
   );
+
+  const handleClick = () => {
+    history.push('/add-view');
+  };
 
   return (
     <div>
       <h1>ListView</h1>
       {error && <p>Error</p>}
       {loading && <p>Loading..</p>}
-      {value && (
+      {!loading && value.docs.length === 0 && (
+        <div>
+          <p>Your shopping list is currently empty</p>
+          <button onClick={handleClick}>Add item</button>
+        </div>
+      )}
+      {value && value.docs.length > 0 && (
         <div>
           Collection:
           <ul>
@@ -24,7 +36,6 @@ function ShowList() {
           </ul>
         </div>
       )}
-
       <Navigation />
     </div>
   );
