c77c00e7b0c942ebea125062bcee4624704391ce
ishimwezachee <zacheeishimwe@gmail.com>
2021-08-03 12:41:41 +0200

    added logic to update the lastPurchased date in firebase

--- src/components/ShowList.jsx
+++ src/components/ShowList.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import db from '../lib/firebase';
 import Navigation from './Navigation';
 import './ShowList.css';
 
 function ShowList() {
+  const [isChecked, setCheck] = useState(false);
   const token = localStorage.getItem('token');
   const [value, loading, error] = useCollection(
     db.collection('items').where('token', '==', token),
   );
+
+  const checkHandler = (e) => {
+    const itemId = e.target.id;
+    // apply the logic of apdating lastPurchasedItem
+    db.collection('items')
+      .doc(itemId)
+      .update({ lastPurchasedDate: new Date() });
+
+    setCheck(true);
+  };
 
   return (
     <div>
@@ -21,7 +32,12 @@ function ShowList() {
           <ul>
             {value.docs.map((doc) => (
               <div className="check-item">
-                <input type="checkbox" />
+                <input
+                  id={doc.id}
+                  type="checkbox"
+                  onChange={checkHandler}
+                  // checked
+                />
                 <li key={doc.id}>{doc.data().name}</li>
               </div>
             ))}
