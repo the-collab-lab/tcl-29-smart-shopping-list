ed9d1fc84bb26d17e83e3a52f64e834c9066b72e
abigailNottingham <abigailillustration@gmail.com>
2021-08-06 17:28:58 +0100

    combined conditional statements for checking of value.docs length and loading with ternary operator

--- src/components/ShowList.jsx
+++ src/components/ShowList.jsx
@@ -20,12 +20,15 @@ function ShowList() {
     <div className="list-view">
       <h1>Smart Shopping List</h1>
       {error && <p>Error</p>}
-      {loading && <p>Loading..</p>}
-      {!loading && value.docs.length === 0 && (
-        <div>
-          <p>Your shopping list is currently empty</p>
-          <button onClick={handleClick}>Add item</button>
-        </div>
+      {loading ? (
+        <p>Loading..</p>
+      ) : (
+        value.docs.length === 0 && (
+          <div>
+            <p>Your shopping list is currently empty</p>
+            <button onClick={handleClick}>Add item</button>
+          </div>
+        )
       )}
       {value && value.docs.length > 0 && (
         <div>
