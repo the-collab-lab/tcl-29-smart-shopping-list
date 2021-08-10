095f06860baa95118bb7342683935bfc735c0603
abigailNottingham <abigailillustration@gmail.com>
2021-08-10 17:30:24 +0100

    added react state to input

--- src/components/ShowList.jsx
+++ src/components/ShowList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import db from '../lib/firebase';
 import { useHistory } from 'react-router-dom';
@@ -6,6 +6,7 @@ import Item from './Item';
 import './ShowList.css';
 
 function ShowList() {
+  const [filter, setFilter] = useState('');
   const token = localStorage.getItem('token');
   const history = useHistory();
   const [value, loading, error] = useCollection(
@@ -31,7 +32,12 @@ function ShowList() {
       )}
       {value && value.docs.length > 0 && (
         <div>
-          <input />
+          <input
+            type="text"
+            placeholder="Filter items..."
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+          />
           <ul>
             {value.docs.map((doc) => (
               <Item key={doc.id} {...doc.data()} id={doc.id} />
